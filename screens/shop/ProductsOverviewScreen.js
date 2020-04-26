492cfd01a81c8f019ee311e58b2e66ae809a0601
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-26 21:43:56 +0400

    added product detail screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -9,13 +9,17 @@ const ProductsOverviewScreen = (props) => {
     return (
         <FlatList
             data={products}
-            keyExtractor={(item) => item.id}
             renderItem={(itemData) => (
                 <ProductItem
                     image={itemData.item.imageUrl}
                     title={itemData.item.title}
                     price={itemData.item.price}
-                    onViewDetail={() => {}}
+                    onViewDetail={() => {
+                        props.navigation.navigate('ProductDetail', {
+                            productId: itemData.item.id,
+                            productTitle: itemData.item.title,
+                        });
+                    }}
                     onAddToCart={() => {}}
                 />
             )}
