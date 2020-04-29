4dcac4874eb3e0728932ec01f7dd421f36dc562e
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-29 14:14:52 +0400

    added admin screen

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -70,6 +70,18 @@ export default (state = initialState, action) => {
             };
         case ADD_ORDER:
             return initialState;
+        case DELETE_PRODUCT:
+            if (!state.items[action.pid]) {
+                return state;
+            }
+            const updatedItems = { ...state.items };
+            const itemTotal = state.items[action.pid].sum;
+            delete updatedItems[action.pid];
+            return {
+                ...state,
+                items: updatedItems,
+                totalAmount: state.totalAmount - itemTotal,
+            };
     }
 
     return state;
